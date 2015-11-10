a85ff1cb25cfa56c29ff3ae23a3283d038d8cc0c
ghaiklor <ghaiklor@gmail.com>
2015-11-10 19:30:00 +0200

    feat(primitive): Implements basic primitives

---
+++ src/primitives/RectanglePrimitive.js
@@ -0,0 +1,11 @@
+import Primitive from './Primitive';
+
+export default class RectanglePrimitive extends Primitive {
+  constructor() {
+    super();
+  }
+
+  render() {
+    let cursor = this.getCursor();
+  }
+}
