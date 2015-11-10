29fdc6a22670efd46443d51426ed78bd8fccbe5b
ghaiklor <ghaiklor@gmail.com>
2015-11-10 19:30:50 +0200

    feat(loader): Implements basic loader

--- src/Loader.js
+++ src/Loader.js
@@ -1,20 +1,30 @@
 import { spawn } from 'child_process';
 
-export class Loader {
+export default class Loader {
   constructor() {
 
   }
 
-  static async getNodeModules() {
-    return new Promise((resolve, reject) => spawn('npm ls --json', (error, stdout, stderr) => error ? reject(error) : resolve(JSON.parse(stdout))));
+  /**
+   * Get all installed node modules
+   * @returns {Promise}
+   */
+  static getNodeModules() {
+    return new Promise((resolve, reject) => {
+      let ls = spawn('npm', ['list', '--json', '-g', '--depth=0']);
+      let modules = '';
+
+      ls.stdout.on('data', data => modules += data);
+      ls.stdout.on('end', () => resolve(modules));
+    });
   }
 
+  /**
+   * Get available themes
+   * @returns {Array}
+   */
   static async getThemes() {
     let modules = await Loader.getNodeModules();
     return modules;
-  }
-
-  static async getPresentations() {
-    return [];
   }
 }
