ec8e0714bc56b815e68960a264a673fe20fefea6
ghaiklor <ghaiklor@gmail.com>
2015-11-20 11:07:25 +0200

    test(shape): Covers with tests serializers

--- test/unit/shapes/Shape.test.js
+++ test/unit/shapes/Shape.test.js
@@ -59,17 +59,103 @@ describe('Shape', () => {
   it('Should properly throw exception if render is not overridden', () => {
     let shape = new Shape();
     assert.throws(() => shape.render(), Error);
+  });
 
-    class Child extends Shape {
-    }
+  it('Should properly serialize shape to object', () => {
+    let shape = new Shape();
+    let obj = shape.toObject();
 
-    let child = new Child();
-    assert.throws(() => child.render(), Error);
+    assert.deepEqual(obj, {
+      name: 'Shape',
+      options: {
+        text: '',
+        width: 15,
+        height: 5,
+        x: 10,
+        y: 10,
+        background: undefined,
+        foreground: undefined
+      }
+    });
+  });
+
+  it('Should properly serialize shape to object with custom options', () => {
+    let shape = new Shape({text: 'test', x: 0, y: 0, width: 30, height: 50});
+    let obj = shape.toObject();
+
+    assert.deepEqual(obj, {
+      name: 'Shape',
+      options: {
+        text: 'test',
+        width: 30,
+        height: 50,
+        x: 0,
+        y: 0,
+        background: undefined,
+        foreground: undefined
+      }
+    });
+  });
+
+  it('Should properly serialize shape to JSON', () => {
+    let shape = new Shape();
+    let json = shape.toJSON();
+
+    assert.equal(json, '{"name":"Shape","options":{"text":"","width":15,"height":5,"x":10,"y":10}}');
+  });
+
+  it('Should properly serialize shape to JSON with custom options', () => {
+    let shape = new Shape({text: 'test', x: 0, y: 0, width: 30, height: 50});
+    let json = shape.toJSON();
+
+    assert.equal(json, '{"name":"Shape","options":{"text":"test","width":30,"height":50,"x":0,"y":0}}');
   });
 
   it('Should properly create Shape instance from static create()', () => {
     let shape = Shape.create({text: 'test'});
     assert.equal(shape.getText(), 'test');
     assert.instanceOf(shape, Shape);
+  });
+
+  it('Should properly throw error if trying to create Shape not from Object representation', () => {
+    let obj = {};
+    assert.throws(() => Shape.fromObject(obj), Error);
+  });
+
+  it('Should properly create Shape instance from Object representation', () => {
+    let obj = {
+      name: 'Shape',
+      options: {
+        text: 'test',
+        width: 30,
+        height: 50,
+        x: 0,
+        y: 0,
+        background: undefined,
+        foreground: undefined
+      }
+    };
+
+    let shape = Shape.fromObject(obj);
+    assert.instanceOf(shape, Shape);
+    assert.equal(shape.getText(), 'test');
+    assert.equal(shape.getWidth(), 30);
+    assert.equal(shape.getHeight(), 50);
+    assert.deepEqual(shape.getPosition(), {x: 0, y: 0});
+    assert.isUndefined(shape.getBackground());
+    assert.isUndefined(shape.getForeground());
+  });
+
+  it('Should properly create Shape instance from JSON representation', () => {
+    let json = '{"name":"Shape","options":{"text":"test","width":30,"height":50,"x":0,"y":0}}';
+    let shape = Shape.fromJSON(json);
+
+    assert.instanceOf(shape, Shape);
+    assert.equal(shape.getText(), 'test');
+    assert.equal(shape.getWidth(), 30);
+    assert.equal(shape.getHeight(), 50);
+    assert.deepEqual(shape.getPosition(), {x: 0, y: 0});
+    assert.isUndefined(shape.getBackground());
+    assert.isUndefined(shape.getForeground());
   });
 });
