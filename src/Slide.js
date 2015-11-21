881b956c6f8672e1f8d291e5b995f7726df1072e
ghaiklor <ghaiklor@gmail.com>
2015-11-22 00:37:35 +0200

    feat(slide): Impplements simple Slide wrapper that allows to render the slide

--- src/Slide.js
+++ src/Slide.js
@@ -1,29 +1,32 @@
-import { Shape } from './shapes/Shape';
+import { Rectangle } from './shapes/Rectangle';
+import { Text } from './shapes/Text';
 
+/**
+ * Creates a new slide with shapes.
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ */
 export class Slide {
+  _shapes = [];
+
   /**
-   * Creates new Slide instance
+   * Creates new Slide instance.
+   *
+   * @param {Array<Shape>} shapes Array of serialized shapes
    * @constructor
    */
-  constructor() {
-    this._shapes = [];
+  constructor(shapes) {
+    this._shapes = shapes.map(shape => {
+      if (shape.name === 'Rectangle') return Rectangle.fromObject(shape.options);
+      if (shape.name === 'Text') return Text.fromObject(shape.options);
+    });
   }
 
   /**
-   * Adds shape to slide
-   * @param {Shape} shape
-   * @returns {Slide}
-   */
-  addShape(shape) {
-    if (!(shape instanceof Shape)) throw new Error('You must provide Shape instance');
-
-    this._shapes.push(shape);
-    return this;
-  }
-
-  /**
-   * Renders the slide
-   * @param {Cursor} cursor
+   * Renders the slide.
+   *
+   * @param {Cursor} cursor Cursor instance which is using for rendering the slide
    * @returns {Slide}
    */
   render(cursor) {
