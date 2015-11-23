4e8d73132ee3d86aeb14db6e07b88e1380b3cb87
ghaiklor <ghaiklor@gmail.com>
2015-11-23 11:20:25 +0200

    feat(presentation): Implements exit method and supports keypress for switching the slides

--- src/Presentation.js
+++ src/Presentation.js
@@ -11,20 +11,29 @@ import { Cursor } from './cursor/Cursor';
  * @version 1.0.0
  */
 export class Presentation {
-  _cursor = Cursor.create([process.stdout], [process.stdin]);
+  _cursor = Cursor.create([process.stdout], [process.stdin]).reset().hide();
   _currentSlideIndex = 0;
   _slides = [];
 
-  constructor() {
+  /**
+   * Creates presentation instance with slides
+   * @param {Array<Array<Object>>} slides
+   */
+  constructor(slides) {
     process.stdin.setRawMode(true);
     process.stdin.setEncoding('utf8');
 
     keypress(process.stdin);
+
+    process.stdin.on('keypress', this._onKeyPress.bind(this));
+
+    this._slides = slides.map(slide => Slide.create(slide));
+    this.renderSlide();
   }
 
   /**
    * Renders specified slide
-   * @param {Number} index Slide index in presentation
+   * @param {Number} [index] Slide index in presentation
    * @returns {Presentation}
    */
   renderSlide(index) {
@@ -48,5 +57,29 @@ export class Presentation {
   prevSlide() {
     this.renderSlide(--this._currentSlideIndex);
     return this;
+  }
+
+  exit() {
+    this._cursor.show().reset();
+    process.exit(0);
+  }
+
+  /**
+   * Triggers when user is pressing the key.
+   *
+   * @param ch
+   * @param key
+   * @private
+   */
+  _onKeyPress(ch, key) {
+    if (key.name == 'left') this.prevSlide();
+    if (key.name == 'right') this.nextSlide();
+    if (key.ctrl && key.name == 'c') this.exit();
+
+    return this;
+  }
+
+  static create(...args) {
+    return new this(...args);
   }
 }
