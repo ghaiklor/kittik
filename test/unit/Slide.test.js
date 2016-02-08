aef2b96661574038bd0c865736ac580c4f431d05
ghaiklor <ghaiklor@gmail.com>
2016-02-08 19:54:43 +0200

    test(slide): Remove wrong tests

--- test/unit/Slide.test.js
+++ test/unit/Slide.test.js
@@ -1,12 +1,28 @@
 import { assert } from 'chai';
-import { Cursor } from 'kittik-cursor';
+import Cursor from 'kittik-cursor';
 import Text from 'kittik-shape-text';
-import { Slide } from '../../src/Slide';
+import Slide from '../../src/Slide';
 
 describe('kittik::Slide', () => {
   it('Should properly create shapes array in the slide', () => {
-    const slide = Slide.create([{type: 'Text', options: {text: 'test'}}]);
-
-    assert.ok(slide._shapes.every(shape => shape instanceof Text));
+    const slide = Slide.create({
+      shapes: [{
+        name: 'Hello',
+        type: 'Text',
+        options: {
+          text: 'Hello, World!'
+        }
+      }],
+      animations: [{
+        name: 'SlideIn',
+        type: 'Slide',
+        options: {
+          direction: 'inRight'
+        }
+      }],
+      flow: [
+        'Hello(SlideIn)'
+      ]
+    });
   });
 });
