5c103d4f77db0e2a9f3f4c6b86d3ed43ba196c60
Eugene Obrezkov <ghaiklor@gmail.com>
2016-04-04 12:51:06 +0300

    chore(package): Fix issue with generating doc for src instead of lib

--- tools/generate_doc.js
+++ tools/generate_doc.js
@@ -6,4 +6,4 @@ const fs = require('fs');
 jsdoc2md({src: './node_modules/kittik-animation-*/**/*.js'}).pipe(fs.createWriteStream('./doc/api/Animation.md'));
 jsdoc2md({src: './node_modules/kittik-shape-*/**/*.js'}).pipe(fs.createWriteStream('./doc/api/Shape.md'));
 jsdoc2md({src: './node_modules/kittik-cursor/**/*.js'}).pipe(fs.createWriteStream('./doc/api/Cursor.md'));
-jsdoc2md({src: './src/**/*.js'}).pipe(fs.createWriteStream('./doc/api/Kittik.md'));
+jsdoc2md({src: './lib/**/*.js'}).pipe(fs.createWriteStream('./doc/api/Kittik.md'));
