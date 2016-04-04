88f2654257e78314a2aa2972315df6ebde1f19f6
Eugene Obrezkov <ghaiklor@gmail.com>
2016-04-04 19:04:39 +0300

    test(slide): Add tests

--- test/unit/Slide.test.js
+++ test/unit/Slide.test.js
@@ -1,104 +1,164 @@
-import { assert } from 'chai';
+import {assert} from 'chai';
+import sinon from 'sinon';
 import Cursor from 'kittik-cursor';
 import Text from 'kittik-shape-text';
 import Print from 'kittik-animation-print';
 import Slide from '../../src/Slide';
 
+const cursor = Cursor.create();
+
+const SLIDE_DECLARATION = {
+  shapes: [{
+    name: 'Hello',
+    type: 'Text',
+    options: {
+      text: 'Hello, World!'
+    }
+  }],
+  animations: [{
+    name: 'Print',
+    type: 'Print',
+    options: {
+      duration: 1
+    }
+  }],
+  order: [
+    'Hello::Print->Print'
+  ]
+};
+
+const SERIALIZED_SLIDE_DECLARATION = {
+  shapes: [{
+    name: 'Hello',
+    type: 'Text',
+    options: {
+      align: 'center',
+      background: false,
+      blink: false,
+      bold: false,
+      dim: false,
+      foreground: false,
+      height: 5,
+      hidden: false,
+      reverse: false,
+      text: 'Hello, World!',
+      underlined: false,
+      width: 15,
+      x: 10,
+      y: 10
+    }
+  }],
+  animations: [{
+    name: 'Print',
+    type: 'Print',
+    options: {
+      duration: 1,
+      easing: 'outQuad'
+    }
+  }],
+  order: [
+    'Hello::Print->Print'
+  ]
+};
+
 describe('kittik::Slide', () => {
-  it('Should properly create shapes array in the slide', () => {
-    const slide = Slide.create({
-      shapes: [{
-        name: 'Hello',
-        type: 'Text',
-        options: {
-          text: 'Hello, World!'
-        }
-      }],
-      animations: [{
-        name: 'SlideIn',
-        type: 'Slide',
-        options: {
-          direction: 'inRight'
-        }
-      }],
-      flow: [
-        'Hello(SlideIn)'
-      ]
+  it('Should properly create slide instance', () => {
+    const slide = Slide.create(cursor, SLIDE_DECLARATION);
+
+    assert.instanceOf(slide._cursor, Cursor);
+    assert.instanceOf(slide._shapes['Hello'], Text);
+    assert.instanceOf(slide._animations['Print'], Print);
+    assert.equal(slide._order.length, 1);
+  });
+
+  it('Should properly build shapes from declaration', () => {
+    const slide = Slide.create(cursor, SLIDE_DECLARATION);
+
+    assert.instanceOf(slide._shapes, Object);
+    assert.instanceOf(slide._shapes['Hello'], Text);
+  });
+
+  it('Should properly build animations from declaration', () => {
+    const slide = Slide.create(cursor, SLIDE_DECLARATION);
+
+    assert.instanceOf(slide._animations, Object);
+    assert.instanceOf(slide._animations['Print'], Print);
+  });
+
+  it('Should properly build order from declaration', () => {
+    const slide = Slide.create(cursor, SLIDE_DECLARATION);
+
+    assert.deepEqual(slide._order, [{shape: 'Hello', animations: ['Print', 'Print']}]);
+  });
+
+  it('Should properly render the specified shapes', () => {
+    const cursor = Cursor.create();
+    const mock = sinon.mock(cursor);
+    const slide = Slide.create(cursor, SLIDE_DECLARATION);
+
+    mock.expects('eraseScreen').once();
+    mock.expects('flush').once();
+
+    slide._renderShapes([slide._shapes['Hello']]);
+
+    mock.verify();
+  });
+
+  it('Should properly render the whole slide', done => {
+    const cursor = Cursor.create();
+    const mock = sinon.mock(cursor);
+    const slide = Slide.create(cursor, SLIDE_DECLARATION);
+
+    mock.expects('write').atLeast(1).returns(cursor);
+    mock.expects('eraseScreen').atLeast(1).returns(cursor);
+    mock.expects('flush').atLeast(1).returns(cursor);
+
+    slide.render().then(() => {
+      mock.verify();
+      done();
     });
   });
 
-  it('Should properly parse shapes', () => {
-    const shapes = [{name: 'Shape', type: 'Text', options: {}}];
-    assert.instanceOf(Slide.parseShapes(shapes), Object);
-    assert.instanceOf(Slide.parseShapes(shapes)['Shape'], Object);
+  it('Should properly serialize slide to the object representation', () => {
+    const slide = Slide.create(cursor, SLIDE_DECLARATION);
+
+    assert.deepEqual(slide.toObject(), SERIALIZED_SLIDE_DECLARATION);
   });
 
-  it('Should properly parse animations', () => {
-    const animations = [{name: 'Anim', type: 'Print', options: {}}];
-    assert.instanceOf(Slide.parseAnimations(animations), Object);
-    assert.instanceOf(Slide.parseAnimations(animations)['Anim'], Object);
-  });
+  it('Should properly serialize slide to JSON representation', () => {
+    const slide = Slide.create(cursor, SLIDE_DECLARATION);
 
-  it('Should properly parse order', () => {
-    assert.deepEqual(Slide.parseOrder(['Shape 1::Anim 1']), [{shape: 'Shape 1', animations: ['Anim 1']}]);
+    assert.deepEqual(JSON.parse(slide.toJSON()), SERIALIZED_SLIDE_DECLARATION);
   });
 
   it('Should properly create Slide instance from static create() method', () => {
-    const slide = Slide.create();
+    const slide = Slide.create(cursor, SLIDE_DECLARATION);
+
     assert.instanceOf(slide, Slide);
+    assert.instanceOf(slide._cursor, Cursor);
+    assert.instanceOf(slide._shapes, Object);
+    assert.instanceOf(slide._shapes['Hello'], Text);
+    assert.instanceOf(slide._animations, Object);
+    assert.instanceOf(slide._animations['Print'], Print);
+    assert.instanceOf(slide._order, Array);
+    assert.deepEqual(slide._order, [{shape: 'Hello', animations: ['Print', 'Print']}]);
   });
 
   it('Should properly create Slide instance from Object representation', () => {
-    const obj = {
-      shapes: [{
-        name: 'Shape 1',
-        type: 'Text',
-        options: {
-          text: 'Test'
-        }
-      }],
-      animations: [{
-        name: 'Print',
-        type: 'Print',
-        options: {
-          duration: 2000
-        }
-      }],
-      order: [
-        'Shape 1::Print'
-      ]
-    };
+    const slide = Slide.fromObject(SERIALIZED_SLIDE_DECLARATION, cursor);
 
-    const slide = Slide.fromObject(obj);
-    assert.instanceOf(slide._shapes['Shape 1'], Text);
+    assert.instanceOf(slide._cursor, Cursor);
+    assert.instanceOf(slide._shapes['Hello'], Text);
     assert.instanceOf(slide._animations['Print'], Print);
-    assert.deepEqual(slide._order[0], {shape: 'Shape 1', animations: ['Print']});
+    assert.deepEqual(slide._order[0], {shape: 'Hello', animations: ['Print', 'Print']});
   });
 
   it('Should properly create Slide instance from JSON representation', () => {
-    const json = JSON.stringify({
-      shapes: [{
-        name: 'Shape 1',
-        type: 'Text',
-        options: {
-          text: 'Test'
-        }
-      }],
-      animations: [{
-        name: 'Print',
-        type: 'Print',
-        options: {
-          duration: 2000
-        }
-      }],
-      order: [
-        'Shape 1::Print'
-      ]
-    });
+    const slide = Slide.fromJSON(JSON.stringify(SERIALIZED_SLIDE_DECLARATION), cursor);
 
-    const slide = Slide.fromJSON(json);
-    assert.instanceOf(slide._shapes['Shape 1'], Text);
+    assert.instanceOf(slide._cursor, Cursor);
+    assert.instanceOf(slide._shapes['Hello'], Text);
     assert.instanceOf(slide._animations['Print'], Print);
-    assert.deepEqual(slide._order[0], {shape: 'Shape 1', animations: ['Print']});
+    assert.deepEqual(slide._order[0], {shape: 'Hello', animations: ['Print', 'Print']});
   });
 });
