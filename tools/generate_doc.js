8298442f5a48325e67bfb2ea266e972f70f977ee
Eugene Obrezkov <ghaiklor@gmail.com>
2016-05-10 13:46:32 +0300

    docs(package): Split Kittik API dos into Slide and Deck files

--- tools/generate_doc.js
+++ tools/generate_doc.js
@@ -6,4 +6,5 @@ const fs = require('fs');
 jsdoc2md({src: './node_modules/kittik-animation-*/**/*.js'}).pipe(fs.createWriteStream('./doc/api/Animation.md'));
 jsdoc2md({src: './node_modules/kittik-shape-*/**/*.js'}).pipe(fs.createWriteStream('./doc/api/Shape.md'));
 jsdoc2md({src: './node_modules/kittik-cursor/**/*.js'}).pipe(fs.createWriteStream('./doc/api/Cursor.md'));
-jsdoc2md({src: './lib/**/*.js'}).pipe(fs.createWriteStream('./doc/api/Kittik.md'));
+jsdoc2md({src: './lib/Deck.js'}).pipe(fs.createWriteStream('./doc/api/Deck.md'));
+jsdoc2md({src: './lib/Slide.js'}).pipe(fs.createWriteStream('./doc/api/Slide.md'));
