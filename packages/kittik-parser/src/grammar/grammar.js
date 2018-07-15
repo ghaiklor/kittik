4dbf2c94dd9644ed3f2fa7a3ae83c37f8da6930d
Eugene Obrezkov <ghaiklor@gmail.com>
2018-07-15 11:38:48 +0300

    Fix issue with string token value, improve whitespace parsing

--- packages/kittik-parser/src/grammar/grammar.js
+++ packages/kittik-parser/src/grammar/grammar.js
@@ -5,13 +5,17 @@ function id(x) { return x[0]; }
 
   const moo = require('moo');
   const lexer = moo.compile({
-    shape: 'shape',
-    animation: 'animation',
     whitespace: { match: /\s+/, lineBreaks: true },
     comment: { match: /#.*?$/ },
-    string: { match: /".*?"/ },
+    string: { match: /".*?"/, lineBreaks: true, value: (value) => value.slice(1, -1) },
     number: { match: /[0-9]+/ },
-    identifier: { match: /[a-zA-Z]+/ },
+    identifier: {
+      match: /[a-zA-Z]+/,
+      keywords: {
+        shape: 'shape',
+        animation: 'animation'
+      }
+    },
     leftParenthesis: '(',
     rightParenthesis: ')',
     colon: ':'
@@ -19,18 +23,24 @@ function id(x) { return x[0]; }
 var grammar = {
     Lexer: lexer,
     ParserRules: [
-    {"name": "deck", "symbols": ["shapeDecl"]},
-    {"name": "deck", "symbols": ["animationDecl"]},
-    {"name": "shapeDecl", "symbols": ["shapeType", "optionDecl"], "postprocess": ([type, options]) => ({name: options.name, type, options})},
+    {"name": "deck", "symbols": ["wso", "shapeDecl", "wso"]},
+    {"name": "deck", "symbols": ["wso", "animationDecl", "wso"]},
+    {"name": "shapeDecl", "symbols": ["shapeType", "wso", "optionDecl"], "postprocess": ([type, options]) => ({name: options.name, type, options})},
     {"name": "shapeType", "symbols": [(lexer.has("shape") ? {type: "shape"} : shape), (lexer.has("colon") ? {type: "colon"} : colon), (lexer.has("identifier") ? {type: "identifier"} : identifier)], "postprocess": ([, , type]) => type.value},
-    {"name": "animationDecl", "symbols": ["animationType", "optionDecl"], "postprocess": ([type, options]) => ({name: options.name, type, options})},
+    {"name": "animationDecl", "symbols": ["animationType", "wso", "optionDecl"], "postprocess": ([type, options]) => ({name: options.name, type, options})},
     {"name": "animationType", "symbols": [(lexer.has("animation") ? {type: "animation"} : animation), (lexer.has("colon") ? {type: "colon"} : colon), (lexer.has("identifier") ? {type: "identifier"} : identifier)], "postprocess": ([, , type]) => type.value},
-    {"name": "optionDecl", "symbols": [(lexer.has("leftParenthesis") ? {type: "leftParenthesis"} : leftParenthesis), "optionList", (lexer.has("rightParenthesis") ? {type: "rightParenthesis"} : rightParenthesis)], "postprocess": ([_, options]) => options},
-    {"name": "optionList", "symbols": ["option", (lexer.has("whitespace") ? {type: "whitespace"} : whitespace), "optionList"], "postprocess": ([lhs, _, rhs]) => Object.assign(lhs, rhs)},
+    {"name": "optionDecl", "symbols": [(lexer.has("leftParenthesis") ? {type: "leftParenthesis"} : leftParenthesis), "wso", "optionList", "wso", (lexer.has("rightParenthesis") ? {type: "rightParenthesis"} : rightParenthesis)], "postprocess": ([, , options]) => options},
+    {"name": "optionList", "symbols": ["option", "wsm", "optionList"], "postprocess": ([lhs, _, rhs]) => Object.assign(lhs, rhs)},
     {"name": "optionList", "symbols": ["option"], "postprocess": id},
     {"name": "optionList", "symbols": []},
-    {"name": "option", "symbols": [(lexer.has("identifier") ? {type: "identifier"} : identifier), (lexer.has("whitespace") ? {type: "whitespace"} : whitespace), (lexer.has("string") ? {type: "string"} : string)], "postprocess": ([key, _, value]) => ({[key]: value.value})},
-    {"name": "option", "symbols": [(lexer.has("identifier") ? {type: "identifier"} : identifier), (lexer.has("whitespace") ? {type: "whitespace"} : whitespace), (lexer.has("number") ? {type: "number"} : number)], "postprocess": ([key, _, value]) => ({[key]: parseInt(value.value)})}
+    {"name": "option", "symbols": [(lexer.has("identifier") ? {type: "identifier"} : identifier), "wsm", (lexer.has("string") ? {type: "string"} : string)], "postprocess": ([key, _, value]) => ({[key]: value.value})},
+    {"name": "option", "symbols": [(lexer.has("identifier") ? {type: "identifier"} : identifier), "wsm", (lexer.has("number") ? {type: "number"} : number)], "postprocess": ([key, _, value]) => ({[key]: parseInt(value.value)})},
+    {"name": "wso$ebnf$1", "symbols": []},
+    {"name": "wso$ebnf$1", "symbols": ["wso$ebnf$1", (lexer.has("whitespace") ? {type: "whitespace"} : whitespace)], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "wso", "symbols": ["wso$ebnf$1"], "postprocess": () => null},
+    {"name": "wsm$ebnf$1", "symbols": [(lexer.has("whitespace") ? {type: "whitespace"} : whitespace)]},
+    {"name": "wsm$ebnf$1", "symbols": ["wsm$ebnf$1", (lexer.has("whitespace") ? {type: "whitespace"} : whitespace)], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "wsm", "symbols": ["wsm$ebnf$1"], "postprocess": () => null}
 ]
   , ParserStart: "deck"
 }
--- packages/kittik-parser/src/grammar/grammar.ne
+++ packages/kittik-parser/src/grammar/grammar.ne
@@ -1,13 +1,17 @@
 @{%
   const moo = require('moo');
   const lexer = moo.compile({
-    shape: 'shape',
-    animation: 'animation',
     whitespace: { match: /\s+/, lineBreaks: true },
     comment: { match: /#.*?$/ },
-    string: { match: /".*?"/ },
+    string: { match: /".*?"/, lineBreaks: true, value: (value) => value.slice(1, -1) },
     number: { match: /[0-9]+/ },
-    identifier: { match: /[a-zA-Z]+/ },
+    identifier: {
+      match: /[a-zA-Z]+/,
+      keywords: {
+        shape: 'shape',
+        animation: 'animation'
+      }
+    },
     leftParenthesis: '(',
     rightParenthesis: ')',
     colon: ':'
@@ -19,13 +23,13 @@
 # rule, that parses the whole input into Deck compatible object
 # the resulting object from this rule can be passed into kittik-deck as is
 deck ->
-    shapeDecl
-  | animationDecl
+    wso shapeDecl wso
+  | wso animationDecl wso
 
 # rule, that parses the whole shape declaration into Deck compatible object
 # i.e. shape:rectangle (name "Hello, World" x 50) will be parsed into kittik-shape-rectangle object
 shapeDecl ->
-    shapeType optionDecl {% ([type, options]) => ({name: options.name, type, options}) %}
+    shapeType wso optionDecl {% ([type, options]) => ({name: options.name, type, options}) %}
 
 # rule, that parses type of shape
 # i.e. shape:rectangle is parsed into "rectangle"
@@ -35,7 +39,7 @@ shapeType ->
 # rule, that parses the whole animation declaration into Deck compatible object
 # i.e. animation:slide (duration 2000) will be parsed into kittik-animation-slide object
 animationDecl ->
-    animationType optionDecl {% ([type, options]) => ({name: options.name, type, options}) %}
+    animationType wso optionDecl {% ([type, options]) => ({name: options.name, type, options}) %}
 
 # rule, that parses type of animation
 # i.e. animation:slide is parsed into "slide"
@@ -43,18 +47,25 @@ animationType ->
     %animation %colon %identifier {% ([, , type]) => type.value %}
 
 # rule, that parses the whole option list in parenthesis into one object
+# so, any key value structures in parenthesis is parsed as an option object
 optionDecl ->
-    %leftParenthesis optionList %rightParenthesis {% ([_, options]) => options %}
+    %leftParenthesis wso optionList wso %rightParenthesis {% ([, , options]) => options %}
 
 # rule, that parses a list of options into one object
 # i.e. (duration 20 name "test") will be parsed into {duration: 20, name: "test"}
 optionList ->
-    option %whitespace optionList {% ([lhs, _, rhs]) => Object.assign(lhs, rhs) %}
+    option wsm optionList {% ([lhs, _, rhs]) => Object.assign(lhs, rhs) %}
   | option {% id %}
   | null
 
 # rule, that parses key value structure into an object
 # i.e. (duration 20) will be parsed into {duration: 20}
 option ->
-    %identifier %whitespace %string {% ([key, _, value]) => ({[key]: value.value}) %}
-  | %identifier %whitespace %number {% ([key, _, value]) => ({[key]: parseInt(value.value)}) %}
+    %identifier wsm %string {% ([key, _, value]) => ({[key]: value.value}) %}
+  | %identifier wsm %number {% ([key, _, value]) => ({[key]: parseInt(value.value)}) %}
+
+# rules, that provide with optional\mandatory whitespaces
+# if you need to have a mandatory whitespace between rules -> wsm
+# but, if you could have whitespaces, but they are optional -> wso
+wso -> %whitespace:* {% () => null %}
+wsm -> %whitespace:+ {% () => null %}
