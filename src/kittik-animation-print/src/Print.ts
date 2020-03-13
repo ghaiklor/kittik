05e64c91ff03ff8b24ad474fc52046d5a56ffbfb
Eugene Obrezkov <ghaiklor@gmail.com>
2020-03-13 19:05:02 +0200

    style: 💄 remove todo comment in animation-print, it works now

--- src/kittik-animation-print/src/Print.ts
+++ src/kittik-animation-print/src/Print.ts
@@ -5,11 +5,6 @@ export class Print extends Animation implements Animationable {
   private originalText = '';
 
   onTick<S extends Shape, P extends keyof S, V extends number>(shape: S, _property: P, value: V): void {
-    // TODO: this case is special, because I know that I'm changing text here only
-    // But I did not figure out, how to explain it to TypeScript yet
-    // It has some issues with generics in basic signature
-    // eslint-disable-next-line
-    // @ts-ignore
     shape.text = this.originalText.slice(0, value);
   }
 
