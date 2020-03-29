b30137dafdc1dec3da0a8d1efb90d11ccde54946
Eugene Obrezkov <ghaiklor@gmail.com>
2020-03-29 19:55:13 +0300

    docs: ✏️ improve examples for SlideBuilder to be more fancy

--- src/kittik-slide/examples/builder.ts
+++ src/kittik-slide/examples/builder.ts
@@ -1,7 +1,9 @@
 import { Canvas } from 'terminal-canvas';
 import { AnimationBuilder, ShapeBuilder, SlideBuilder } from '..';
+import { SlideOptions } from 'kittik-animation-slide';
+import { FocusOptions } from 'kittik-animation-focus';
 
-const cursor = Canvas.create().saveScreen().reset().hideCursor();
+const cursor = Canvas.create().reset().hideCursor();
 
 SlideBuilder
   .start()
@@ -10,31 +12,42 @@ SlideBuilder
     'Hello, World',
     ShapeBuilder
       .start('Rectangle')
-      .withText('Hello, World')
+      .withText('Hello, World!')
       .withBackground('aqua')
-      .withForeground('green')
+      .withForeground('black')
       .withX('center')
       .withY('middle')
       .end()
   )
-  .withShape(
-    'Footer',
-    ShapeBuilder
-      .start('FigText')
-      .withText('kittik (c) ghaiklor')
-      .withX('center')
-      .withY('bottom')
+  .withAnimation(
+    'Slide In From Left',
+    AnimationBuilder
+      .start('Slide')
+      .withDuration(5000)
+      .withEasing('inBounce')
+      .withOptions({ direction: 'inLeft' } as SlideOptions)
       .end()
   )
   .withAnimation(
-    'Printing',
+    'Focusing',
     AnimationBuilder
-      .start('Print')
+      .start('Focus')
+      .withDuration(1000)
+      .withEasing('inOutSine')
+      .withOptions({ direction: 'shakeX', repeat: 3 } as FocusOptions)
       .end()
   )
-  .withOrder('Hello, World', ['Printing'])
-  .withOrder('Footer', ['Printing'])
+  .withAnimation(
+    'Slide Out To Right',
+    AnimationBuilder
+      .start('Slide')
+      .withDuration(5000)
+      .withEasing('outBounce')
+      .withOptions({ direction: 'outRight' } as SlideOptions)
+      .end()
+  )
+  .withOrder('Hello, World', ['Slide In From Left', 'Focusing', 'Slide Out To Right'])
   .end()
   .render()
-  .finally(() => cursor.restoreScreen().showCursor())
+  .finally(() => cursor.reset().showCursor())
   .catch(e => console.error(e));
---
+++ src/kittik-slide/examples/declaration.ts
@@ -0,0 +1,70 @@
+import { AnimationDeclaration } from '../src/animation/AnimationDeclaration';
+import { Canvas } from 'terminal-canvas';
+import { ShapeDeclaration } from '../src/shape/ShapeDeclaration';
+import { Slide } from '../dist/slide/Slide';
+import { SlideOptions } from 'kittik-animation-slide';
+
+const cursor = Canvas.create().reset().hideCursor();
+
+const HelloShapeDeclaration: ShapeDeclaration = {
+  name: 'Hello',
+  type: 'Rectangle',
+  options: {
+    x: 'center',
+    y: 'middle',
+    background: 'aqua',
+    foreground: 'black',
+    text: 'Hello, World!'
+  }
+};
+
+const PrintAnimationDeclaration: AnimationDeclaration = {
+  name: 'Print',
+  type: 'Print',
+  options: {
+    duration: 5000,
+    easing: 'inOutCubic'
+  }
+};
+
+const SlideInLeftAnimationOptions: Partial<SlideOptions> = {
+  direction: 'inLeft',
+  duration: 5000,
+  easing: 'inBounce'
+};
+
+const SlideInLeftAnimationDeclaration: AnimationDeclaration = {
+  name: 'Slide In Left',
+  type: 'Slide',
+  options: SlideInLeftAnimationOptions
+};
+
+const SlideOutRightAnimationOptions: Partial<SlideOptions> = {
+  direction: 'outRight',
+  duration: 5000,
+  easing: 'outBounce'
+};
+
+const SlideOutRightAnimationDeclaration: AnimationDeclaration = {
+  name: 'Slide Out Right',
+  type: 'Slide',
+  options: SlideOutRightAnimationOptions
+};
+
+const slide = new Slide(cursor, {
+  shapes: [HelloShapeDeclaration],
+  animations: [PrintAnimationDeclaration, SlideInLeftAnimationDeclaration, SlideOutRightAnimationDeclaration],
+  order: [{
+    shape: 'Hello',
+    animations: [
+      'Slide In Left',
+      'Print',
+      'Slide Out Right'
+    ]
+  }]
+});
+
+slide
+  .render()
+  .finally(() => cursor.reset().showCursor())
+  .catch(e => console.error(e));
