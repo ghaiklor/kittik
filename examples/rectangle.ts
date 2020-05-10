5621ee37de0fa9e06d66c1c600389e2f1d1080f7
Eugene Obrezkov <ghaiklor@gmail.com>
2020-05-10 15:25:56 +0300

    docs: ✏️ add a little example for DeckBuilder in root examples

---
+++ examples/rectangle.ts
@@ -0,0 +1,25 @@
+import { DeckBuilder, ShapeBuilder } from '../packages/kittik-deck';
+
+DeckBuilder
+  .start()
+  .withSlide(
+    (builder) => builder
+      .withShape(
+        'Name of the Shape to reference',
+        ShapeBuilder
+          .start('Rectangle')
+          .withBackground('aqua')
+          .withForeground('green')
+          .withHeight('40%')
+          .withText('Hello, World!')
+          .withWidth('80%')
+          .withX('center')
+          .withY('middle')
+          .end()
+      )
+      .withOrder('Name of the Shape to reference')
+      .end()
+  )
+  .end()
+  .render()
+  .catch((error) => console.error(error));
